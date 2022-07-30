d31e22ea090e0bf2f099b3bc452c466493e52fae
Arad Firouzkouhi <aradfir@gmail.com>
2022-07-30 15:40:39 +0430

    refactored maze game functions

--- main.cpp
+++ main.cpp
@@ -27,58 +27,74 @@ Tensor mazeToTensor(vector<vector<int>> maze, int pos[2]) {
 
 }
 
-void mazeGame() {
+void print_maze(const vector<vector<int>> &maze) {
+    for (int i = 0; i < 8; ++i) {
+        for (int j = 0; j < 8; ++j) {
+            cout << maze[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+vector<vector<int>> init_maze(int pos[2], int end[2]) {
     vector<vector<int>> maze(8, vector<int>(8, 0));
-    int pos[2] = {0, 0};
-    int end[2] = {7, 7};
     //maze[0][6]=-1;
     maze[pos[0]][pos[1]] = 1;
     maze[end[0]][end[1]] = 2;
+    return maze;
+}
+bool reached_fin(int pos[2],int end[2]){
+    return pos[0] == end[0] && pos[1] == end[1];
+}
+double do_action(vector<vector<int>> &maze,int &action,int pos[2],int end[2])
+{
+    maze[pos[0]][pos[1]] = 0;
+    double reward = -5;
+    if (action == 0) {
+        pos[0]--;
+        if (pos[0] < 0)
+            pos[0] = 0;
+    } else if (action == 1) {
+        pos[0]++;
+        if (pos[0] > 7)
+            pos[0] = 7;
+        else reward = 10;
+    } else if (action == 2) {
+        pos[1]--;
+        if (pos[1] < 0)
+            pos[1] = 0;
+    } else if (action == 3) {
+        pos[1]++;
+        if (pos[1] > 7)
+            pos[1] = 7;
+        else reward = 10;
+    }
+    maze[pos[0]][pos[1]] = 1;
+    if (reached_fin(pos,end))
+        reward = 100;
+    return reward;
+
+}
+void mazeGame() {
+    int pos[2] = {0, 0};
+    int end[2] = {7, 7};
+    auto maze = init_maze(pos, end);
     Tensor t = mazeToTensor(maze, pos);
 
     string out_message;
-    Tensor *res = con.send_tensor_data("QLearning_sample","init_state","",t,out_message);
+    Tensor *res = con.send_tensor_data("QLearning_sample", "init_state", "", t, out_message);
     cout << out_message << string(*res) << endl;
     int steps = 0;
-    while (pos[0] != end[0] || pos[1] != end[1]) {
-        res = con.send_tensor_data("QLearning_sample","get_action","",t,out_message);
-        for (int i = 0; i < 8; ++i) {
-            for (int j = 0; j < 8; ++j) {
-                cout << maze[i][j] << " ";
-            }
-            cout << endl;
-        }
+    while (!reached_fin(pos,end)) {
+        res = con.send_tensor_data("QLearning_sample", "get_action", "", t, out_message);
+        print_maze(maze);
         int action = (*res)[{0}];
         cout << out_message << " " << action << endl;
-        maze[pos[0]][pos[1]] = 0;
-        double reward = -5;
-        if (action == 0) {
-            pos[0]--;
-            if (pos[0] < 0)
-                pos[0] = 0;
-        } else if (action == 1) {
-            pos[0]++;
-            if (pos[0] > 7)
-                pos[0] = 7;
-            else reward = 10;
-        } else if (action == 2) {
-            pos[1]--;
-            if (pos[1] < 0)
-                pos[1] = 0;
-        } else if (action == 3) {
-            pos[1]++;
-            if (pos[1] > 7)
-                pos[1] = 7;
-            else reward = 10;
-        }
-        maze[pos[0]][pos[1]] = 1;
-
-        if (pos[0] == end[0] && pos[1] == end[1])
-            reward = 100;
+        double reward= do_action(maze,action,pos,end);
         t = mazeToTensor(maze, pos);
-        bool is_fin = pos[0] == end[0] && pos[1] == end[1];
+        bool is_fin = reached_fin(pos,end);
         string reward_fin = to_string(reward) + " " + to_string(is_fin);
-        res = con.send_tensor_data("QLearning_sample","get_reward_new_state",reward_fin,t,out_message);
+        res = con.send_tensor_data("QLearning_sample", "get_reward_new_state", reward_fin, t, out_message);
         cout << out_message << string(*res) << endl;
         steps++;
     }
